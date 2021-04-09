6a340800471e4cc936afda99619f5dbb1d850163
Joel Cripps <jcripps@fortinet.com>
2021-04-09 09:04:11 -0700

    changing vars to capitals

--- instance.tf
+++ instance.tf
@@ -4,40 +4,41 @@ data "ibm_is_ssh_key" "ssh_key" {
 
 resource "ibm_is_volume" "logDisk1" {
   // Name must be lower case
-  name    = "${var.cluster_name}-logdisk1-${random_string.random_suffix.result}"
+  name    = "${var.CLUSTER_NAME}-logdisk1-${random_string.random_suffix.result}"
   profile = "10iops-tier"
   zone    = var.zone1
 }
 
 resource "ibm_is_volume" "logDisk2" {
   // Name must be lower case
-  name    = "${var.cluster_name}-logdisk2-${random_string.random_suffix.result}"
+  name    = "${var.CLUSTER_NAME}-logdisk2-${random_string.random_suffix.result}"
   profile = "10iops-tier"
   zone    = var.zone2
 }
 
 resource "ibm_is_floating_ip" "publicip" {
-  name   = "${var.cluster_name}-publicip-${random_string.random_suffix.result}"
+  name   = "${var.CLUSTER_NAME}-publicip-${random_string.random_suffix.result}"
   target = ibm_is_instance.fgt1.primary_network_interface[0].id
 }
 
 //Primary Fortigate
 resource "ibm_is_instance" "fgt1" {
-  name    = "${var.cluster_name}-fortigate1-${random_string.random_suffix.result}"
+  name    = "${var.CLUSTER_NAME}-fortigate1-${random_string.random_suffix.result}"
   image   = ibm_is_image.vnf_custom_image.id
-  profile = var.profile
+  profile = var.PROFILE
 
   primary_network_interface {
-    name            = "${var.cluster_name}-port1-${random_string.random_suffix.result}"
-    subnet          = data.ibm_is_subnet.subnet1.id
-    security_groups = [data.ibm_is_security_group.fgt_security_group.id]
-        primary_ipv4_address = var.primary_ipv4_fgt1
+    name                 = "${var.CLUSTER_NAME}-port1-${random_string.random_suffix.result}"
+    subnet               = data.ibm_is_subnet.subnet1.id
+    security_groups      = [data.ibm_is_security_group.fgt_security_group.id]
+    primary_ipv4_address = var.FGT1_STATIC_IP_PORT1
   }
 
   network_interfaces {
-    name                 = "${var.cluster_name}-port2-${random_string.random_suffix.result}"
+    name                 = "${var.CLUSTER_NAME}-port2-${random_string.random_suffix.result}"
     subnet               = data.ibm_is_subnet.subnet2.id
     security_groups      = [data.ibm_is_security_group.fgt_security_group.id]
+    primary_ipv4_address = var.FGT1_STATIC_IP_PORT2
 
 
   }
@@ -47,26 +48,29 @@ resource "ibm_is_instance" "fgt1" {
   vpc       = data.ibm_is_vpc.vpc1.id
   zone      = var.zone1
   user_data = data.template_file.userdata_active.rendered
-  keys      = [data.ibm_is_ssh_key.ssh_key.id]]
+  keys      = [data.ibm_is_ssh_key.ssh_key.id]
+
 }
+
 // Secondary FortiGate
 resource "ibm_is_instance" "fgt2" {
-  name    = "${var.cluster_name}-fortigate2-${random_string.random_suffix.result}"
+  name    = "${var.CLUSTER_NAME}-fortigate2-${random_string.random_suffix.result}"
   image   = ibm_is_image.vnf_custom_image.id
-  profile = var.profile
+  profile = var.PROFILE
 
   primary_network_interface {
-    name            = "${var.cluster_name}-port1-${random_string.random_suffix.result}"
-    subnet          = data.ibm_is_subnet.zone_two_subnet_1.id
-    security_groups = [data.ibm_is_security_group.fgt_security_group.id]
-    primary_ipv4_address =var.primary_ipv4_fgt2
+    name                 = "${var.CLUSTER_NAME}-port1-${random_string.random_suffix.result}"
+    subnet               = data.ibm_is_subnet.zone_two_subnet_1.id
+    security_groups      = [data.ibm_is_security_group.fgt_security_group.id]
+    primary_ipv4_address = var.FGT2_STATIC_IP_PORT1
   }
 
   network_interfaces {
-    name   = "${var.cluster_name}-port2-${random_string.random_suffix.result}"
-       subnet          = data.ibm_is_subnet.zone_two_subnet_2.id
+    name   = "${var.CLUSTER_NAME}-port2-${random_string.random_suffix.result}"
+    subnet = data.ibm_is_subnet.zone_two_subnet_2.id
 
     security_groups      = [data.ibm_is_security_group.fgt_security_group.id]
+    primary_ipv4_address = var.FGT2_STATIC_IP_PORT2
 
   }
 
@@ -75,7 +79,7 @@ resource "ibm_is_instance" "fgt2" {
   vpc       = data.ibm_is_vpc.vpc1.id
   zone      = var.zone2
   user_data = data.template_file.userdata_passive.rendered
-  keys      = [data.ibm_is_ssh_key.ssh_key.id]]
+  keys      = [data.ibm_is_ssh_key.ssh_key.id]
 }
 
 // Use for bootstrapping cloud-init
@@ -84,11 +88,12 @@ resource "ibm_is_instance" "fgt2" {
 data "template_file" "userdata_active" {
   template = file("user_data_active.conf")
   vars = {
-    fgt_1_active_ip  = var.primary_ipv4_fgt1
-    fgt_2_passive_ip = var.primary_ipv4_fgt2
-    netmask          = "255.255.255.0"
-    ibm_api_key = var.ibmcloud_api_key
-    region = var.region
+    fgt_1_active_ip       = var.FGT1_STATIC_IP_PORT1
+    fgt_2_passive_ip      = var.FGT1_STATIC_IP_PORT2
+    fgt_1_active_ip_port2 = var.FGT1_STATIC_IP_PORT2 == "" ? "" : "set ip ${var.FGT1_STATIC_IP_PORT2} ${var.NETMASK}" // if left blank, no ip set. If IP then it is set.
+    netmask               = var.NETMASK
+    ibm_api_key           = var.IBMCLOUD_API_KEY
+    region                = var.region
   }
 }
 
@@ -96,10 +101,11 @@ data "template_file" "userdata_active" {
 data "template_file" "userdata_passive" {
   template = file("user_data_passive.conf")
   vars = {
-    fgt_1_active_ip  = var.primary_ipv4_fgt1
-    fgt_2_passive_ip =var.primary_ipv4_fgt2
-    netmask          = "255.255.255.0"
-    ibm_api_key = var.ibmcloud_api_key
-    region = var.region
+    fgt_1_active_ip        = var.FGT1_STATIC_IP_PORT1
+    fgt_2_passive_ip       = var.FGT1_STATIC_IP_PORT2
+    fgt_2_passive_ip_port2 = var.FGT2_STATIC_IP_PORT2 == "" ? "" : "set ip ${var.FGT2_STATIC_IP_PORT2} ${var.NETMASK}" // if left blank, no ip set. If IP then it is set.
+    netmask                = var.NETMASK
+    ibm_api_key            = var.IBMCLOUD_API_KEY
+    region                 = var.region
   }
 }
--- vpc.tf
+++ vpc.tf
@@ -3,28 +3,28 @@
 # }
 
 data "ibm_is_vpc" "vpc1" {
-  name = var.vpc
+  name = var.VPC
 }
 data "ibm_is_subnet" "subnet1" {
-  name = var.subnet1
+  name = var.ZONE1_SUBNET_1
 }
 data "ibm_is_subnet" "subnet2" {
-  name = var.subnet2
+  name = var.ZONE1_SUBNET_2
 }
 data "ibm_is_subnet" "zone_two_subnet_1" {
-  name = var.zone_two_subnet_1
+  name = var.ZONE2_SUBNET_1
 }
 data "ibm_is_subnet" "zone_two_subnet_2" {
-  name = var.zone_two_subnet_2
+  name = var.ZONE2_SUBNET_2
 }
 
 
-resource "ibm_is_vpc_routing_table" "fgt_route_table" {
+resource "ibm_is_vpc_routing_table" "fgt_route_table" {q
   name = "${var.cluster_name}-port1-${random_string.random_suffix.result}"
   vpc  = data.ibm_is_vpc.vpc1.id
 }
 
 
 data "ibm_is_security_group" "fgt_security_group" {
-  name = var.security_group
+  name = var.SECURITY_GROUP
 }
