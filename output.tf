a3b5ac925af0f1debdac029c0b6b4f7e9577404d
Joel Cripps <jcripps@fortinet.com>
2021-04-12 18:16:21 -0700

    added New ports and subnets. heartbeat is now a seperate port

--- output.tf
+++ output.tf
@@ -1,7 +1,12 @@
 output "FortiGate_Public_IP" {
   value = ibm_is_floating_ip.publicip.address
 }
-
+output "FGT2_Public_HA_Mangment_IP" {
+  value = ibm_is_floating_ip.publicip2.address
+}
+output "FGT1_Public_HA_Mangment_IP" {
+  value = ibm_is_floating_ip.publicip3.address
+}
 output "Custom_Image_Name" {
   description = "Your local FortiGate Custom Image reference"
   value       = ibm_is_image.vnf_custom_image.name
@@ -10,6 +15,9 @@ output "Username" {
   value = "admin"
 }
 
-output "Default_Admin_Password" {
+output "FGT1_Default_Admin_Password" {
   value = ibm_is_instance.fgt1.id
+}
+output "FGT2_Default_Admin_Password" {
+  value = ibm_is_instance.fgt2.id
 }
