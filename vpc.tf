cb56daa069b52dd04a05c850c8dac8ac988a1ebd
Farazi Shuva <fshuva@fortinet.com>
2024-08-08 10:24:52 -0700

    update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -21,66 +21,66 @@ data "ibm_is_security_group" "fgt_security_group" {
 locals {
   active = {
     "interface1" = {
-      ip = var.FGT1_STATIC_IP_PORT1,
+      ip     = var.FGT1_STATIC_IP_PORT1,
       subnet = var.SUBNET_1
     },
     "interface2" = {
-      ip = var.FGT1_STATIC_IP_PORT2,
+      ip     = var.FGT1_STATIC_IP_PORT2,
       subnet = var.SUBNET_2
     },
     "interface3" = {
-      ip = var.FGT1_STATIC_IP_PORT3,
+      ip     = var.FGT1_STATIC_IP_PORT3,
       subnet = var.SUBNET_3
     },
     "interface4" = {
-      ip = var.FGT1_STATIC_IP_PORT4,
+      ip     = var.FGT1_STATIC_IP_PORT4,
       subnet = var.SUBNET_4
     },
   }
-    passive = {
+  passive = {
     "interface1" = {
-      ip = var.FGT2_STATIC_IP_PORT1,
+      ip     = var.FGT2_STATIC_IP_PORT1,
       subnet = var.SUBNET_1
     },
     "interface2" = {
-      ip = var.FGT2_STATIC_IP_PORT2,
+      ip     = var.FGT2_STATIC_IP_PORT2,
       subnet = var.SUBNET_2
     },
     "interface3" = {
-      ip = var.FGT2_STATIC_IP_PORT3,
+      ip     = var.FGT2_STATIC_IP_PORT3,
       subnet = var.SUBNET_3
     },
     "interface4" = {
-      ip = var.FGT2_STATIC_IP_PORT4,
+      ip     = var.FGT2_STATIC_IP_PORT4,
       subnet = var.SUBNET_4
     },
   }
-  
+
 }
 
 resource "ibm_is_virtual_network_interface" "vni-active" {
-  for_each =  local.active
-  name = "${var.CLUSTER_NAME}-${each.key}-${random_string.random_suffix.result}-${each.key}"
-  allow_ip_spoofing = false
-  auto_delete = false
+  for_each                  = local.active
+  name                      = "${var.CLUSTER_NAME}-fgt1-${each.key}-${random_string.random_suffix.result}"
+  allow_ip_spoofing         = false
+  auto_delete               = false
   enable_infrastructure_nat = true
-  security_groups = [data.ibm_is_security_group.fgt_security_group.id]
+  security_groups           = [data.ibm_is_security_group.fgt_security_group.id]
   primary_ip {
     auto_delete = false
-    address = each.value.ip
+    address     = each.value.ip
   }
-    subnet = each.value.subnet
+  subnet = each.value.subnet
 }
 
 resource "ibm_is_virtual_network_interface" "vni-passive" {
-  for_each =  local.passive
-  name = "${var.CLUSTER_NAME}-${each.key}-${random_string.random_suffix.result}-${each.key}"
-  allow_ip_spoofing = false
-  auto_delete = false
+  for_each                  = local.passive
+  name                      = "${var.CLUSTER_NAME}-fgt2-${each.key}-${random_string.random_suffix.result}"
+  allow_ip_spoofing         = false
+  auto_delete               = false
   enable_infrastructure_nat = true
   primary_ip {
     auto_delete = false
-    address = each.value.ip
+    address     = each.value.ip
   }
-    subnet = each.value.subnet
+  subnet = each.value.subnet
 }
