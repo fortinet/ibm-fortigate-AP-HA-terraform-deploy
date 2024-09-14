bbdbc085b176be979a3242fcca763be2900e8104
Farazi Shuva <fshuva@fortinet.com>
2024-09-13 22:20:53 -0700

    fixing vni reference

--- vpc.tf
+++ vpc.tf
@@ -87,7 +87,7 @@ resource "ibm_is_subnet_reserved_ip" "primary-fgt-ips" {
 
 resource "ibm_is_virtual_network_interface_ip" "vni-reserved-ip-active" {
   for_each = local.active
-  virtual_network_interface = ibm_is_virtual_network_interface.vni-active[each.key]
+  virtual_network_interface = ibm_is_virtual_network_interface.vni-active[each.key].id
   reserved_ip = ibm_is_subnet_reserved_ip.primary-fgt-ips[each.key].reserved_ip
 }
 
@@ -100,6 +100,6 @@ resource "ibm_is_subnet_reserved_ip" "secondary-fgt-ips" {
 
 resource "ibm_is_virtual_network_interface_ip" "vni-reserved-ip-passive" {
   for_each = local.passive
-  virtual_network_interface = ibm_is_virtual_network_interface.vni-passive[each.key]
+  virtual_network_interface = ibm_is_virtual_network_interface.vni-passive[each.key].id
   reserved_ip = ibm_is_subnet_reserved_ip.secondary-fgt-ips[each.key].reserved_ip
 }
