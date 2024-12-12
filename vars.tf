f86b5733aa925c1beca5c44c98d89cfc47d38858
Farazi Shuva <fshuva@fortinet.com>
2024-12-12 12:12:48 -0800

    Updating to FortiOS v7.6.1B3457

--- vars.tf
+++ vars.tf
@@ -162,10 +162,10 @@ resource "random_string" "random_suffix" {
 }
 
 // FortiOS Custom Image ID
-// https://docs.fortinet.com/document/fortigate-public-cloud/7.0.0/ibm-cloud-administration-guide/324064/ha-for-fortigate-vm-on-ibm-cloud
-//Deploys 7.6.0 Image
+// https://docs.fortinet.com/document/fortigate-public-cloud/7.6.0/ibm-cloud-administration-guide/992669/deploying-fortigate-vm-on-ibm-cloud
+// Deploys 7.6.1 image
 variable "image" {
-  default = "cos://us-geo/fortinet/fortigate_byol_760_b3401_GA.qcow2"
+  default = "cos://us-geo/fortinet/fortigate_byol_761_b3457_GA.qcow2"
 }
 
 variable "IBMCLOUD_API_KEY" {
