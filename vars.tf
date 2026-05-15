e17835324b68640ad1084864cc2c48c2d22dd10a
Ricky Ho <rho@Ricky-Hos-MBP.lan>
2026-05-14 18:37:40 -0700

    Upgrade to FortiOS v8.0.0 B0167

--- vars.tf
+++ vars.tf
@@ -162,7 +162,7 @@ resource "random_string" "random_suffix" {
 }
 
 // FortiOS Custom Image ID
-// https://docs.fortinet.com/document/fortigate-public-cloud/7.6.0/ibm-cloud-administration-guide/992669/deploying-fortigate-vm-on-ibm-cloud
+// https://docs.fortinet.com/document/fortigate-public-cloud/latest/ibm-cloud-administration-guide/992669/deploying-fortigate-vm-on-ibm-cloud
 // Deploys 8.0.0 image
 variable "image" {
   default = "cos://us-geo/fortinet/fortigate_byol_800_b0167_GA.qcow2"
