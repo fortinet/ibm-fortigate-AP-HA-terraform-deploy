c3fc47ed8c664b6b2e94290a072cb14df4a4a6d7
Farazi Shuva <fshuva@fortinet.com>
2026-07-21 11:20:52 -0700

    adding a rule for ha talk

--- vpc.tf
+++ vpc.tf
@@ -17,12 +17,12 @@ data "ibm_is_subnet" "subnet4" {
 
 data "ibm_is_security_group" "fgt_security_group_public" {
   count = var.SECURITY_GROUP_PUBLIC != "" ? 1 : 0
-  name = var.SECURITY_GROUP_PUBLIC
+  name  = var.SECURITY_GROUP_PUBLIC
 }
 
 data "ibm_is_security_group" "fgt_security_group_private" {
   count = var.SECURITY_GROUP_PRIVATE != "" ? 1 : 0
-  name = var.SECURITY_GROUP_PRIVATE
+  name  = var.SECURITY_GROUP_PRIVATE
 }
 
 resource "ibm_is_security_group" "fgt_security_group_public" {
@@ -38,55 +38,64 @@ resource "ibm_is_security_group" "fgt_security_group_private" {
 }
 
 locals {
-  security_group_public_id = var.SECURITY_GROUP_PUBLIC != "" ? data.ibm_is_security_group.fgt_security_group_public[0].id : ibm_is_security_group.fgt_security_group_public[0].id
-  security_group_private_id = var.SECURITY_GROUP_PRIVATE != "" ? data.ibm_is_security_group.fgt_security_group_private[0].id : ibm_is_security_group.fgt_security_group_private[0].id
-  security_group_public_name = var.SECURITY_GROUP_PUBLIC != "" ? data.ibm_is_security_group.fgt_security_group_public[0].name : ibm_is_security_group.fgt_security_group_public[0].name
+  security_group_public_id    = var.SECURITY_GROUP_PUBLIC != "" ? data.ibm_is_security_group.fgt_security_group_public[0].id : ibm_is_security_group.fgt_security_group_public[0].id
+  security_group_private_id   = var.SECURITY_GROUP_PRIVATE != "" ? data.ibm_is_security_group.fgt_security_group_private[0].id : ibm_is_security_group.fgt_security_group_private[0].id
+  security_group_public_name  = var.SECURITY_GROUP_PUBLIC != "" ? data.ibm_is_security_group.fgt_security_group_public[0].name : ibm_is_security_group.fgt_security_group_public[0].name
   security_group_private_name = var.SECURITY_GROUP_PRIVATE != "" ? data.ibm_is_security_group.fgt_security_group_private[0].name : ibm_is_security_group.fgt_security_group_private[0].name
 
+}
+
+#Rule to enable HA talk
+resource "ibm_is_security_group_rule" "internal_traffic" {
+  group = local.security_group_private_id
+
+  direction = "inbound"
+  local     = "0.0.0.0/0"
+  remote    = local.security_group_private_id
 }
 
 locals {
   active = {
     "interface1" = {
-      ip     = var.FGT1_STATIC_IP_PORT1,
-      subnet = var.SUBNET_1
+      ip             = var.FGT1_STATIC_IP_PORT1,
+      subnet         = var.SUBNET_1
       security_group = local.security_group_public_id
     },
     "interface2" = {
-      ip     = var.FGT1_STATIC_IP_PORT2,
-      subnet = var.SUBNET_2
+      ip             = var.FGT1_STATIC_IP_PORT2,
+      subnet         = var.SUBNET_2
       security_group = local.security_group_private_id
     },
     "interface3" = {
-      ip     = var.FGT1_STATIC_IP_PORT3,
-      subnet = var.SUBNET_3
+      ip             = var.FGT1_STATIC_IP_PORT3,
+      subnet         = var.SUBNET_3
       security_group = local.security_group_private_id
     },
     "interface4" = {
-      ip     = var.FGT1_STATIC_IP_PORT4,
-      subnet = var.SUBNET_4
+      ip             = var.FGT1_STATIC_IP_PORT4,
+      subnet         = var.SUBNET_4
       security_group = local.security_group_public_id
     },
   }
   passive = {
     "interface1" = {
-      ip     = var.FGT2_STATIC_IP_PORT1,
-      subnet = var.SUBNET_1
+      ip             = var.FGT2_STATIC_IP_PORT1,
+      subnet         = var.SUBNET_1
       security_group = local.security_group_public_id
     },
     "interface2" = {
-      ip     = var.FGT2_STATIC_IP_PORT2,
-      subnet = var.SUBNET_2
+      ip             = var.FGT2_STATIC_IP_PORT2,
+      subnet         = var.SUBNET_2
       security_group = local.security_group_private_id
     },
     "interface3" = {
-      ip     = var.FGT2_STATIC_IP_PORT3,
-      subnet = var.SUBNET_3
+      ip             = var.FGT2_STATIC_IP_PORT3,
+      subnet         = var.SUBNET_3
       security_group = local.security_group_private_id
     },
     "interface4" = {
-      ip     = var.FGT2_STATIC_IP_PORT4,
-      subnet = var.SUBNET_4
+      ip             = var.FGT2_STATIC_IP_PORT4,
+      subnet         = var.SUBNET_4
       security_group = local.security_group_public_id
     },
   }
