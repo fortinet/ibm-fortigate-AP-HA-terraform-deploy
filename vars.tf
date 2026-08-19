a90efaf38656eac3a81519ec973a5bc064e95df3
root <rho@fortinet.com>
2026-08-19 11:55:37 -0700

    Upgrade to FortiOS v8 B0237

--- vars.tf
+++ vars.tf
@@ -163,9 +163,9 @@ resource "random_string" "random_suffix" {
 
 // FortiOS Custom Image ID
 // https://docs.fortinet.com/document/fortigate-public-cloud/latest/ibm-cloud-administration-guide/992669/deploying-fortigate-vm-on-ibm-cloud
-// Deploys 7.6.7 image
+// Deploys 8 image
 variable "image" {
-  default = "cos://us-geo/fortinet/fortigate_byol_767_b3704_GA.qcow2"
+  default = "cos://us-geo/fortinet/fortigate_byol_8_b0237_Interim.qcow2"
 }
 
 variable "IBMCLOUD_API_KEY" {
